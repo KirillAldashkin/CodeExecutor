8c49037dfa89aca0d75b6e6231fb228ef5fa0a27
KirillAldashkin <a89086520092k@yandex.ru>
2023-03-16 13:04:30 +0800

    Some chance command tweaks

--- Services/FunService.cs
+++ Services/FunService.cs
@@ -6,13 +6,14 @@ internal class FunService
 {
     public FunService(DiscordService discord)
     {
-        discord.AddHandler("шанс", GetChance);
+        discord.AddHandler("шанс", GetChance("Шанс"));
+        discord.AddHandler("вероятность", GetChance("Вероятность"));
     }
 
-    private async Task GetChance(ReadOnlyMemory<char> text, DiscordMessage message)
+    private DiscordService.CommandHandler GetChance(string prefix) => async (text, message) =>
     {
         var what = string.Create(text.Length, text, (s, m) => m.Span.CopyTo(s));
         var chance = System.Security.Cryptography.RandomNumberGenerator.GetInt32(0, 1000);
-        await message.RespondAsync($"Шанс того, что {what} составляет {chance/10}.{chance%10}%");
-    }
+        await message.RespondAsync($"{prefix} {what} составляет {chance / 10}.{chance % 10}%");
+    };
 }
